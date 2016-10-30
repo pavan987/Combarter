54ca7d0b0d0a57b9e86b8e8e6401f1beaa01fe81
pavan987 <gondhi@usc.edu>
2016-10-30 02:00:08 -0700

    added /api/me, /api/profile , /api/join

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 
 users = {
-    "id":1,
+    "id":'1',
     "first_name":"Sherlock",
     "last_name":"Holmes",
     "email":"sherlock@example.com",
@@ -20,7 +20,7 @@ users = {
 buildings = { 'buildings': [
 {
    "id":'1',
-   "name":"Mardi Gras",
+   "title":"Mardi Gras",
    "address":"720 W 27th Street",
    "city":"Los Angeles",
    "state":"CA",
@@ -31,7 +31,7 @@ buildings = { 'buildings': [
 
 {
    "id":'2',
-   "name":"Nupac Apartments",
+   "title":"Nupac Apartments",
    "address":"450 W 28th Street",
    "city":"San Diego",
    "state":"CA",
@@ -43,7 +43,7 @@ buildings = { 'buildings': [
 
 {
    "id":'3',
-   "name":"First Choice Hosuing",
+   "title":"First Choice Hosuing",
    "address":"908 W Adams Blvd",
    "city":"Los Angeles",
    "state":"CA",
@@ -55,7 +55,7 @@ buildings = { 'buildings': [
 
 {
    "id":'4',
-   "name":"Stuho",
+   "title":"Stuho",
    "address":"2650 Orhchard Avenue",
    "city":"Pasadena",
    "state":"CA",
@@ -67,7 +67,7 @@ buildings = { 'buildings': [
 
 {
    "id":'5',
-   "name":"Nupac Apartments",
+   "title":"Nupac Apartments",
    "address":"2656 Ellendale",
    "city":"Sacramento",
    "state":"CA",
@@ -81,15 +81,40 @@ buildings = { 'buildings': [
 ]
 }
 
+join = {}
+my_profile={}
 
-
+temp_carpool = {
+    "results" : [
+        {
+            "id":1,
+            "where":"Viva Technologies, 26th W Street",
+            "when":"2016-30-10",
+            "max_occupancy":3,
+            "owner":{
+                "id":"2",
+                "first_name":"Mehul",
+                "last_name":"Lispov"
+            }
+        },
+        {
+            "id": 2,
+            "where":"2652 Ellendale Place",
+            "when":"2016-02-11",
+            "max_occupancy":5,
+            "owner":{
+                "id":"3",
+                "first_name": "Matt",
+                "last_name": "Damon"
+            }
+        }
+    ]
+}
 
 def abort_if_apartment_doesnt_exist(apt_id):
     if apt_id not in buildings:
         abort(404, message="Apartment {} doesn't exist".format(apt_id))
 
-parser = reqparse.RequestParser()
-parser.add_argument('task')
 
 
 # shows a single todo item and lets you delete a todo item
@@ -102,26 +127,65 @@ class Apt(Resource):
 class AptList(Resource):
     def get(self):
         return buildings
-'''
-class user(Resource):
+
+class UserJoin(Resource):
+    def get(self):
+        return join
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user')
+        parser.add_argument('building')
+        parser.add_argument('status')
+        args = parser.parse_args()
+        for key, value in args.items():
+            if value == None:
+                return {"status":"failure","message":"Check all parameters user, building, status are sent "}
+            join[key] = value
+        if args['status']=='approved':
+            for b in buildings['buildings']:
+                if args['building'] == b['id']:
+                    my_profile['building']=b
+                    my_profile['user']=users
+                    return my_profile
+
+        return {"status":"success","message":"Request sent successfully!"}
+
+class MyProfile(Resource):
+    def get(self):
+        return my_profile
+
+class User(Resource):
     def get(self):
         return users
 
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('first_name')
+        parser.add_argument('last_name')
+        parser.add_argument('email')
+        parser.add_argument('apt')
         args = parser.parse_args()
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-'''
+        print args
+        for key,value in args.items():
+            if value:
+                users[key]=value
+        return {"status":"success"}
+
 
 class BuildingSearch(Resource):
     def get(self,name):
         search_results = {'status':'failure','results':[]}
         flag = 0
-        for value in buildings['buildings']:
-            if name in value['id'] or name.lower() in value['name'].lower() or name.lower() in value['city'].lower() \
-                    or name.lower() in value['state'].lower() or  name in value['zip']:
-                flag = 1
-                search_results["results"].append(value)
+        if name.strip() == '':
+            flag = 1
+            search_results['status'] ='success'
+            search_results["results"]= buildings['buildings']
+        else:
+            for value in buildings['buildings']:
+                if name in value['id'] or name.lower() in value['title'].lower() or name.lower() in value['city'].lower() \
+                        or name.lower() in value['state'].lower() or  name in value['zip']:
+                    flag = 1
+                    search_results["results"].append(value)
         if flag == 1:
             search_results['status'] ='success'
         return search_results
@@ -131,7 +195,8 @@ class BuildingSearch(Resource):
 api.add_resource(AptList, '/api')
 api.add_resource(Apt, '/api/<apt_id>')
 api.add_resource(BuildingSearch, '/api/search/<name>')
-
-
+api.add_resource(User, '/api/profile')
+api.add_resource(UserJoin, '/api/join')
+api.add_resource(MyProfile, '/api/me')
 if __name__ == '__main__':
     app.run(debug=True)
